20362e80cd4b1953a815791062c80873eae59439
weeger <romain.weeger@gmail.com>
2023-04-28 17:46:32 +0200

    First functions

--- src/Api/Controller/AbstractApiController.php
+++ src/Api/Controller/AbstractApiController.php
@@ -2,9 +2,61 @@
 
 namespace Wexample\SymfonyApi\Api\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Wexample\SymfonyApi\Helper\ApiHelper;
 
-class AbstractApiController extends AbstractController
+abstract class AbstractApiController extends AbstractController
 {
-    
+    public static function apiResponseSuccess(
+        $message = null,
+        $data = [],
+        $status = ApiHelper::RESPONSE_TYPE_SUCCESS,
+    ): JsonResponse {
+        return self::apiResponse(
+            $message,
+            $status,
+            $data
+        );
+    }
+
+    public static function apiResponse(
+        $message = null,
+        $type = ApiHelper::RESPONSE_TYPE_SUCCESS,
+        $data = []
+    ): JsonResponse {
+        $status = ApiHelper::RESPONSE_TYPE_FAILURE === $type
+            ? Response::HTTP_BAD_REQUEST : Response::HTTP_OK;
+
+        $dataDefault = [
+            ApiHelper::KEY_RESPONSE_TYPE => $type,
+            ApiHelper::KEY_RESPONSE_STATUS => $status,
+        ];
+
+        if (!is_null($message)) {
+            $dataDefault[ApiHelper::KEY_RESPONSE_MESSAGE] = $message;
+        }
+
+        return new JsonResponse(
+            array_merge(
+                $dataDefault,
+                $data
+            ),
+            $status
+        );
+    }
+
+    public static function apiResponseError(
+        string|Exception $message,
+        $data = [],
+        $type = ApiHelper::RESPONSE_TYPE_FAILURE,
+    ): JsonResponse {
+        return self::apiResponse(
+            $message instanceof Exception ? $message->getMessage() : $message,
+            $type,
+            $data
+        );
+    }
 }
---
+++ src/Helper/ApiHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wexample\SymfonyApi\Helper;
+
+class ApiHelper
+{
+    public const RESPONSE_TYPE_SUCCESS = 'success';
+
+    public const RESPONSE_TYPE_FAILURE = 'error';
+
+    public const KEY_RESPONSE_TYPE = 'type';
+
+    public const KEY_RESPONSE_STATUS = 'status';
+
+    public const KEY_RESPONSE_MESSAGE = 'message';
+}
