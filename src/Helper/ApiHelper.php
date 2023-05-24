eb486d85fd72581e4cf7c660077072ef28681a81
weeger <romain.weeger@gmail.com>
2023-05-24 11:12:07 +0200

    Sorting

--- src/Helper/ApiHelper.php
+++ src/Helper/ApiHelper.php
@@ -4,13 +4,13 @@ namespace Wexample\SymfonyApi\Helper;
 
 class ApiHelper
 {
-    public const RESPONSE_TYPE_SUCCESS = 'success';
-
     public const RESPONSE_TYPE_FAILURE = 'error';
 
-    public const KEY_RESPONSE_TYPE = 'type';
+    public const KEY_RESPONSE_MESSAGE = 'message';
 
     public const KEY_RESPONSE_STATUS = 'status';
 
-    public const KEY_RESPONSE_MESSAGE = 'message';
+    public const RESPONSE_TYPE_SUCCESS = 'success';
+
+    public const KEY_RESPONSE_TYPE = 'type';
 }
