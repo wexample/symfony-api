5c76505366369f088128607ae46cf9b6b701333b
weeger <romain.weeger@gmail.com>
2023-06-15 10:03:46 +0200

    Cleanup

--- .wex/script/ciCheck.sh
+++ .wex/script/ciCheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-_wexLog "Running custom app scripts"
+echo "Running custom app scripts"
 # List and sort the tags
 ALL_TAGS=$(git tag -l | sort -V)
 
@@ -13,8 +13,8 @@ rm -rf .env.skip
 
 # Check if CURRENT_VERSION is the latest
 if [ "$CURRENT_VERSION" != "$LATEST_TAG" ]; then
-    _wexLog "The current version ($CURRENT_VERSION) is not the latest tag.. stopping pipeline"
+    echo "The current version ($CURRENT_VERSION) is not the latest tag.. stopping pipeline"
     echo "SKIP_BUILD=true" >> .env.skip
 else
-    _wexLog "The CURRENT_VERSION ($CURRENT_VERSION) is the latest tag.. continue"
+    echo "The CURRENT_VERSION ($CURRENT_VERSION) is the latest tag.. continue"
 fi
