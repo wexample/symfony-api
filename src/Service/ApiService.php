509fd0da4142092b8ae7b9bd84b1d1ee7cca9c51
weeger <romain.weeger@gmail.com>
2023-06-23 09:36:02 +0200

    Improve info page

---
+++ src/Service/ApiService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wexample\SymfonyApi\Service;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Wexample\SymfonyDesignSystem\Translation\Translator;
+use Wexample\SymfonyHelpers\Helper\TextHelper;
+
+readonly class ApiService
+{
+    public function __construct(
+        private RequestStack $requestStack,
+        private UrlGeneratorInterface $router,
+        private Translator $translator,
+        private RouterInterface $symfonyRouter
+    ) {
+    }
+
+    public function buildExampleUrl(string $routeName): string
+    {
+        // Get expected route parameters
+        $route = $this->symfonyRouter->getRouteCollection()->get($routeName);
+        $parameters = $route->compile()->getPathVariables();
+
+        $routeParameters = [];
+
+        foreach ($parameters as $parameter) {
+            $snakeName = TextHelper::toSnake($parameter);
+            $routeParameters[$parameter] = $this->translator->trans(
+                id:'example.'.$snakeName,
+                domain:'api.'.$routeName,
+            );
+        }
+
+        return $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost().
+            $this->router->generate($routeName, $routeParameters);
+    }
+}
---
+++ src/Twig/ApiExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Wexample\SymfonyApi\Twig;
+
+use Twig\TwigFunction;
+use Wexample\SymfonyApi\Service\ApiService;
+use Wexample\SymfonyDesignSystem\Twig\AbstractExtension;
+
+class ApiExtension extends AbstractExtension
+{
+    public function __construct(
+        readonly private ApiService $apiService
+    ) {
+
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction(
+                'api_build_example_url',
+                [
+                    $this,
+                    'apiBuildExampleUrl',
+                ]
+            ),
+        ];
+    }
+
+    public function apiBuildExampleUrl(string $routeName): string
+    {
+        return $this->apiService->buildExampleUrl($routeName);
+    }
+}
