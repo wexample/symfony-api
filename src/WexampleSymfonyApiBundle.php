44986c351a486c36a84000a59cddde76c35771da
weeger <romain.weeger@gmail.com>
2023-06-28 11:00:06 +0200

    Allow aliases in fronts paths

--- src/WexampleSymfonyApiBundle.php
+++ src/WexampleSymfonyApiBundle.php
@@ -4,13 +4,14 @@ namespace Wexample\SymfonyApi;
 
 use Wexample\SymfonyDesignSystem\Interface\DesignSystemBundleInterface;
 use Wexample\SymfonyHelpers\AbstractBundle;
+use Wexample\SymfonyHelpers\Helper\BundleHelper;
 
 class WexampleSymfonyApiBundle extends AbstractBundle implements DesignSystemBundleInterface
 {
     public static function getDesignSystemFrontPaths(): array
     {
         return [
-            __DIR__.'/../assets/',
+            BundleHelper::getBundleCssAlias(static::class) => __DIR__.'/../assets/',
         ];
     }
 }
