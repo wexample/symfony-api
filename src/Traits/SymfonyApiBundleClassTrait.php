a2eff46caf199a5c84d80bbc2f69b52ee96dbde6
weeger <romain.weeger@gmail.com>
2024-02-06 17:21:30 +0100

    Refactor

--- src/Traits/SymfonyApiBundleClassTrait.php
+++ src/Traits/SymfonyApiBundleClassTrait.php
@@ -3,9 +3,12 @@
 namespace Wexample\SymfonyApi\Traits;
 
 use Wexample\SymfonyApi\WexampleSymfonyApiBundle;
+use Wexample\SymfonyHelpers\Traits\BundleClassTrait;
 
 trait SymfonyApiBundleClassTrait
 {
+    use BundleClassTrait;
+
     public static function getBundleClassName(): string
     {
         return WexampleSymfonyApiBundle::class;
