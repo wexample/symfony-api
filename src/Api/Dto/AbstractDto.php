c318971ab01362e115badb5165cef360e4f74460
weeger <romain.weeger@gmail.com>
2024-10-25 17:17:17 +0200

    Required

--- src/Api/Dto/AbstractDto.php
+++ src/Api/Dto/AbstractDto.php
@@ -2,12 +2,30 @@
 
 namespace Wexample\SymfonyApi\Api\Dto;
 
+use ReflectionClass;
 use Symfony\Component\Validator\Constraints\Collection;
+use Wexample\SymfonyApi\Api\Attribute\RequiredDtoProperty;
 
 abstract class AbstractDto
 {
     public static function getConstraints(): ?Collection
     {
         return null;
+    }
+
+    public static function getRequiredProperties(): array
+    {
+        $requiredProperties = [];
+        $reflectionClass = new ReflectionClass(static::class);
+
+        foreach ($reflectionClass->getProperties() as $property) {
+            $attributes = $property->getAttributes(RequiredDtoProperty::class);
+
+            if (!empty($attributes)) {
+                $requiredProperties[] = $property->getName();
+            }
+        }
+
+        return $requiredProperties;
     }
 }
