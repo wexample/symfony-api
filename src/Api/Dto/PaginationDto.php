387dd5d14d743565b9a2188cc78d90090fbcb1e0
weeger <romain.weeger@gmail.com>
2024-11-12 14:35:14 +0100

    Various

---
+++ src/Api/Dto/PaginationDto.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Wexample\SymfonyApi\Api\Dto;
+
+use ReflectionClass;
+use Symfony\Component\Validator\Constraints\Collection;
+use Wexample\SymfonyApi\Api\Attribute\RequiredDtoProperty;
+
+class PaginationDto
+{
+
+}
