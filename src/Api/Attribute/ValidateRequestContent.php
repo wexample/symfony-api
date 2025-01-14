7800175103571f6dba09c9bf8f75ef04d0d52cc7
weeger <romain.weeger@gmail.com>
2025-01-14 14:14:59 +0100

    Store files

--- src/Api/Attribute/ValidateRequestContent.php
+++ src/Api/Attribute/ValidateRequestContent.php
@@ -7,10 +7,23 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 class ValidateRequestContent
 {
+    /** @var string[] */
+    private array $dataFieldNames;
+
     public function __construct(
         public string $dto,
         public string $attributeName = 'content',
+        string|array $dataFieldNames = 'data'
     ) {
+        $this->dataFieldNames = is_array($dataFieldNames) ? $dataFieldNames : [$dataFieldNames];
+    }
 
+    /**
+     * Get the field names to look for data in multipart requests
+     * @return string[]
+     */
+    public function getDataFieldNames(): array
+    {
+        return $this->dataFieldNames;
     }
 }
