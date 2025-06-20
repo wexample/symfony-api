66f2c6660c45db42e0756fa82b34e8676139ca29
weeger <romain.weeger@gmail.com>
2025-06-20 12:12:26 +0200

    Trace

--- src/Helper/DebugHelper.php
+++ src/Helper/DebugHelper.php
@@ -11,10 +11,10 @@ class DebugHelper
     {
         $output = \Wexample\SymfonyHelpers\Helper\DebugHelper::formatVar($var);
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
-        $location = $trace['file'].':'.$trace['line'];
+        $location = $trace['file'] . ':' . $trace['line'];
 
-        echo "\n=== DEBUG (".$location.") ===\n";
-        echo $output."\n";
+        echo "\n=== DEBUG (" . $location . ") ===\n";
+        echo $output . "\n";
         echo "==============================\n";
     }
 
@@ -33,5 +33,10 @@ class DebugHelper
     public static function dd(mixed $var): never
     {
         static::dumpAndDie($var);
+    }
+
+    public static function trace(bool $ignoreArgs = true): void
+    {
+        static::dump(debug_backtrace($ignoreArgs ? DEBUG_BACKTRACE_IGNORE_ARGS : null));
     }
 }
