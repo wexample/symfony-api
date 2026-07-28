38bdbf6c88caed9bb240e675a40779078fe58c50
weeger <romain.weeger@gmail.com>
2026-07-28 19:14:49 +0200

    Cleanup

--- src/Api/Dto/AbstractEntityDto.php
+++ src/Api/Dto/AbstractEntityDto.php
@@ -2,6 +2,17 @@
 
 namespace Wexample\SymfonyApi\Api\Dto;
 
+use Wexample\SymfonyHelpers\Entity\AbstractEntity;
+use Wexample\SymfonyMoney\Entity\Currency;
+
 abstract class AbstractEntityDto extends AbstractDto
 {
+    public string $secureId;
+
+    public static function fromEntity(AbstractEntity $entity): self
+    {
+        $dto = new static();
+        $dto->secureId = $entity->getSecureId();
+        return $dto;
+    }
 }
